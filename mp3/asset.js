0a7c06add34ad95e86484b960bb43ccb316e19f5
udarakr <udarakr@gmail.com>
2013-11-21 10:04:45 +0530

    adding resources

---
+++ mp3/asset.js
@@ -0,0 +1,17 @@
+var assetLinks = function (user) {
+    return {
+        title: 'MP3s',
+        links: [
+            {
+                title: 'MP3s',
+                url: ''
+            },
+            {
+                title: 'Bookmarks',
+                url: 'bookmarks',
+                path: 'bookmarks.jag'
+            }
+        ],
+        isCategorySupport: true
+    };
+};
